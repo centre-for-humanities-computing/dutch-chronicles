00a55b25f89e32c0810ac72c05b86323897d0826
supplyandcommand <42962106+supplyandcommand@users.noreply.github.com>
2022-03-08 17:09:49 +0100

    separate segmentation strategy 1

---
+++ chronicles/parser/date_delimited_documents.py
@@ -0,0 +1,132 @@
+'''
+Document segmentation into primitives, where documents are delimited by dates.
+
+Text corresponding to a primitive -> `{date_1} document_1 {date_2} document_2`
+
+'''
+# %%
+import os
+import re
+from wasabi import msg
+
+import bs4
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+import lxml
+
+
+def detect_date_locator(tag):
+    '''
+    Help function, finding key to extract date tag from an annotated field.
+
+    line : bs4.element.Tag
+        can behave like a list 
+    '''
+    # get correct tag to use
+    if tag.has_attr('datum'):
+        locator = 'datum'
+    elif tag.has_attr('when'):
+        locator = 'when'
+    else:
+        locator = None
+    
+    return locator
+
+def extract_date_attr(tag):
+    '''
+    Help function, actually extract date labels
+
+    line : bs4.element.Tag
+        can behave like a list 
+    '''
+
+    # extract, depending if list or not
+    dates = []
+    for date in tag:
+        locator = detect_date_locator(date)
+        if locator:
+            dates.append(date[locator])
+
+    return dates
+
+
+def delimitation_experiment1(increments):
+    '''
+    text corresponding to a primitive -> `{date_1} document_1 {date_2} document_2`
+
+    Parameters
+    ----------
+    increment : bs4.element.ResultSet
+        iterable of bs4.element.Tag
+    '''
+    # check input is not completely outlandish
+    assert(isinstance(increments, bs4.element.ResultSet))
+    assert(isinstance(increments[0], bs4.element.Tag))
+
+    docs = [] # dump for the results
+    partial_doc = {} # create an empty extraction doc that gives a False
+    for i, line in enumerate(increments):
+
+        # detect dates in line
+        dates_found = line.find_all('datum')
+
+        # if datum is found, start a new doc
+        if dates_found:
+            # use date tag extraction function to handle exception
+            doc_date = extract_date_attr(dates_found)
+
+            # add cache (PREVIOUS doc) to results
+            docs.append(partial_doc)
+
+            # TODO
+            # here there should be splitting of the text in a single line
+            # text before date tag -> old partial_doc
+            # text after date tag -> new partial_doc
+
+            # start a NEW doc (bc date was found)
+            partial_doc = {}
+            text_content = [i.get_text() for i in line]
+            partial_doc['date'] = doc_date
+            partial_doc['text'] = text_content
+
+            # last increment in the file
+            # sometimes needs to be added to last document worked on manually
+            # this happens, when last increment contains a date
+            #     a new partial doc is created in if dates_found:
+            #     but, it is never added to the results list
+            if i+1 == len(increments):
+                docs.append(partial_doc)
+
+        else:
+            # if there's something in cache 
+            # aka PREVIOUS doc is not empty = being worked on
+            if partial_doc:
+                # add text lines to it
+                text_content = [i.get_text() for i in line]
+                partial_doc['text'].extend(text_content)
+            
+            # if there's nothing in cache
+            # this happens for first lines of a file, before a date has appears
+            else:
+                # start a fresh doc/cache (seems unnessssary)
+                partial_doc = {}
+                # extract and add
+                text_content = [i.get_text() for i in line]
+                # fake date. TODO input something else?
+                partial_doc['date'] = 'NaN_before_date'
+                partial_doc['text'] = text_content
+
+    return docs
+
+
+
+path = '/Users/au582299/Repositories/dutch-chronicles/data/corpus_220222_corrected/1791_Purm_Louw_03.xml'
+
+with open(path, 'r') as f_in:
+    soup = BeautifulSoup(f_in, 'lxml')
+
+increments = soup.find_all('l')
+res = delimitation_experiment1(increments)
+# 1366
+
+# %%
--- chronicles/parser/parse_chronicles.py
+++ chronicles/parser/parse_chronicles.py
@@ -6,6 +6,7 @@ import numpy as np
 import re
 from wasabi import msg
 
+import bs4
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 import lxml
@@ -36,210 +37,183 @@ bs4 parsing
 #     docs_lxml.append(wordstring_without_hyphen)
 
 # %%
+# ahoj
+def detect_date_locator(tag):
+    '''
+    line : bs4.element.Tag
+        can behave like a list 
+    '''
+    # get correct tag to use
+    if tag.has_attr('datum'):
+        locator = 'datum'
+    elif tag.has_attr('when'):
+        locator = 'when'
+    else:
+        locator = None
+    
+    return locator
+
+def extract_date_attr(tag):
+    '''
+    line : bs4.element.Tag
+        can behave like a list 
+    '''
+
+    # extract, depending if list or not
+    dates = []
+    for date in tag:
+        locator = detect_date_locator(date)
+        if locator:
+            dates.append(date[locator])
+
+    return dates
+
+
+# %%
 ###
-### one doc: extract primitives
+### one case document parser – experiment 1
 ###
-'''
-- extract title
-- extract {document_unit}
-    - extract text
-        - join linebreaks ¬
-    - extract tags
-        - datum
-        - person
-        - loc
-'''
-document_increment = 'p'
+
 path = '/Users/au582299/Repositories/dutch-chronicles/data/corpus_220222_corrected/1791_Purm_Louw_03.xml'
 
 with open(path, 'r') as f_in:
     soup = BeautifulSoup(f_in, 'lxml')
 
-    # extract call_nr: {YYYY}_{LOCATION_TAG}_{AUTHOR_TAG}
-    title_tags = soup.find_all('title')
-    # check only one call_nr is present in file
-    assert len(title_tags) == 1
-    call_nr = title_tags[0].get_text()
+    increments = soup.find_all('l')
 
-    # page numbers
-    page_nrs = [page_nr['n'] for page_nr in soup.find_all('pb')]
+# if you solve it for increment[2], then we're doing good
 
-    primitives = []
-    for page_nr, increment in zip(page_nrs, soup.find_all(document_increment)):
-        text_lines = [line.get_text() for line in increment.find_all('l')]
+# increment_lines = increments[3].find_all('l')
 
-        # catch multiple dates in a single increment
-        dates_in_increment = increment.find_all('datum')
-        if isinstance(dates_in_increment, list):
+docs = []
+partial_doc = {}
+for i, line in enumerate(increments):
 
-            date_lines = []
-            for line in dates_in_increment:
-                if line.has_attr('datum'):
-                    date_tag_type = 'datum'
-                elif line.has_attr('when'):
-                    date_tag_type = 'when'
-                
-                one_date = line[date_tag_type]
-                date_lines.append(one_date)
+    # detect dates in line
+    # TODO: finding 'when'
+    dates_found = line.find_all('datum')
 
+    # if datum is found, start a new doc
+    if dates_found:
+        doc_date = extract_date_attr(dates_found)
+
+        # add old doc
+        docs.append(partial_doc)
+
+        # start a new doc
+        partial_doc = {}
+        text_content = [i.get_text() for i in line]
+        partial_doc['date'] = doc_date
+        partial_doc['text'] = text_content
+
+    else:
+        if partial_doc:
+            text_content = [i.get_text() for i in line]
+            partial_doc['text'].extend(text_content)
         else:
-            date_lines = dates_in_increment['datum']
+            partial_doc = {}
+            text_content = [i.get_text() for i in line]
+            partial_doc['date'] = 'NaN'
+            partial_doc['text'] = text_content
 
-        # date_lines = [line['datum'] for line in increment.find_all('datum')]
-        # person_lines = date_lines = [line.get_text() for line in increment.find_all('person')]
-        # loc_lines = date_lines = [line.get_text() for line in increment.find_all('loc')]
+    if i+1 == len(increments):
+        docs.append(partial_doc)
 
-        primitives.append({
-            'call_nr': call_nr,
-            'page': page_nr,
-            'text': text_lines,
-            'date': date_lines
-        })
+'''
+Segment on lines, not pages (events can go between pages)
 
+Missing last documents
+'''
+# %%
 
 # %%
-def extract_primitives(path, document_increment):
+def delimitation_experiment1(increment):
     '''
-    Get all date annotations from a chronicle
+    text corresponding to a primitive -> `{date_1} document_1 {date_2} document_2`
+
+    Parameters
+    ----------
+    increment : bs4.element.ResultSet
+        iterable of bs4.element.Tag
     '''
+
+    assert(isinstance(increment, bs4.element.ResultSet))
+    assert(isinstance(increment[0], bs4.element.Tag))
+
+    docs = []
+    partial_doc = {}
+    for i, line in enumerate(increments):
+
+        # detect dates in line
+        # TODO: finding 'when'
+        dates_found = line.find_all('datum')
+
+        # if datum is found, start a new doc
+        if dates_found:
+            doc_date = extract_date_attr(dates_found)
+
+            # add old doc
+            docs.append(partial_doc)
+
+            # start a new doc
+            partial_doc = {}
+            text_content = [i.get_text() for i in line]
+            partial_doc['date'] = doc_date
+            partial_doc['text'] = text_content
+
+        else:
+            if partial_doc:
+                text_content = [i.get_text() for i in line]
+                partial_doc['text'].extend(text_content)
+            else:
+                partial_doc = {}
+                text_content = [i.get_text() for i in line]
+                partial_doc['date'] = 'NaN'
+                partial_doc['text'] = text_content
+
+        if i+1 == len(increments):
+            docs.append(partial_doc)
+
+
+        return docs
+
+
+
+def chroincle_loader(path, increment_tag):
 
     with open(path, 'r') as f_in:
         soup = BeautifulSoup(f_in, 'lxml')
 
-        # extract call_nr: {YYYY}_{LOCATION_TAG}_{AUTHOR_TAG}
-        title_tags = soup.find_all('title')
-        # check only one call_nr is present in file
-        assert len(title_tags) == 1
-        call_nr = title_tags[0].get_text()
-
-        # page numbers
-        page_nrs = [page_nr['n'] for page_nr in soup.find_all('pb')]
-
-        primitives = []
-        for page_nr, increment in zip(page_nrs, soup.find_all(document_increment)):
-            text_lines = [line.get_text() for line in increment.find_all('l')]
-
-            # catch multiple dates in a single increment
-            dates_in_increment = increment.find_all('datum')
-            if isinstance(dates_in_increment, list):
-
-                date_lines = []
-                for line in dates_in_increment:
-                    if line.has_attr('datum'):
-                        date_tag_type = 'datum'
-                    elif line.has_attr('when'):
-                        date_tag_type = 'when'
-                    else: 
-                        # exception when date is tagged, but not annotated
-                        date_tag_type = None
-                    
-                    if date_tag_type:
-                        one_date = line[date_tag_type]
-                        date_lines.append(one_date)
-
-            else:
-                date_lines = dates_in_increment['datum']
-
-            # date_lines = [line['datum'] for line in increment.find_all('datum')]
-            # person_lines = date_lines = [line.get_text() for line in increment.find_all('person')]
-            # loc_lines = date_lines = [line.get_text() for line in increment.find_all('loc')]
-
-            primitives.append({
-                'call_nr': call_nr,
-                'page': page_nr,
-                'text': text_lines,
-                'date': date_lines
-            })
-    
-    return primitives
-
-
-# get filepaths
-data_dir = '../../data/corpus_220222_corrected'
-xml_paths = [os.path.join(data_dir, path)
-             for path in os.listdir(data_dir) if path.endswith('.xml')]
-
-chronicles = []
-for path in tqdm(xml_paths):
-    msg.info(path)
-    chron = extract_primitives(path, document_increment='p')
-    chronicles.append(chron)
-
-# %%
-###
-### document parser
-# ###
-# def delimitation_experiment_1(path):
-#     '''
-#     Document segmentation / delimitation: Experiment 1
-
-#     text corresponding to a primitive -> {date_1} document_1 {date_2} document_2
-#     '''
-
-path = '/Users/au582299/Repositories/dutch-chronicles/data/corpus_220222_corrected/1791_Purm_Louw_03.xml'
-
-with open(path, 'r') as f_in:
-    soup = BeautifulSoup(f_in, 'lxml')
-
-    increments = soup.find_all('p')
-
-# if you solve it for increment[2], then we're doing good
-
-increment_lines = increments[2].find_all('l')
-
-docs = []
-for t in increment_lines:
-    doc = []
-    # if datum is found, start a new doc
-    if t.find('datum'):
-        next
-    else:
-        doc.append(t)
-
-    docs.extend(doc)
-
-
-# %%
-### code to be used bellow
-
-# %%
     # extract call_nr: {YYYY}_{LOCATION_TAG}_{AUTHOR_TAG}
     title_tags = soup.find_all('title')
-    # check only one call_nr is present in file
-    assert len(title_tags) == 1
+    # one call_nr must be present in file (see tests/test_parsing)
     call_nr = title_tags[0].get_text()
 
-    # page numbers
+    # get a list of page numbers. len(page_nrs) is the number of pages in a chronicle.
     page_nrs = [page_nr['n'] for page_nr in soup.find_all('pb')]
 
-    primitives = []
-    for page_nr, increment in zip(page_nrs, soup.find_all(document_increment)):
-        text_lines = [line.get_text() for line in increment.find_all('l')]
+    # get document increments to itterate over
+    increments = soup.find_all(increment_tag)
 
-        # catch multiple dates in a single increment
-        dates_in_increment = increment.find_all('datum')
-        if isinstance(dates_in_increment, list):
+    # iterate over page numbers & increments
+    primitives_chronicle = []
+    for page_nr, increment in zip(page_nrs, increments):
+        doc = delimitation_experiment1(increment.find_all('l'))
+        primitives_chronicle.extend(doc)
+    
+    return primitives_chronicle
 
-            date_lines = []
-            for line in dates_in_increment:
-                if line.has_attr('datum'):
-                    date_tag_type = 'datum'
-                elif line.has_attr('when'):
-                    date_tag_type = 'when'
-                else: 
-                    # exception when date is tagged, but not annotated
-                    date_tag_type = None
-                
-                if date_tag_type:
-                    one_date = line[date_tag_type]
-                    date_lines.append(one_date)
 
-        else:
-            date_lines = dates_in_increment['datum']
+# %%
+# test run: works for just lines?
+path = '/Users/au582299/Repositories/dutch-chronicles/data/corpus_220222_corrected/1791_Purm_Louw_03.xml'
+incr_tag = 'l'
+a = chroincle_loader(path, incr_tag)
 
-        primitives.append({
-            'call_nr': call_nr,
-            'page': page_nr,
-            'text': text_lines,
-            'date': date_lines
-        })
+# %%
+# test run: works for pages?
+path = '/Users/au582299/Repositories/dutch-chronicles/data/corpus_220222_corrected/1791_Purm_Louw_03.xml'
+incr_tag = 'p'
+a = chroincle_loader(path, incr_tag)
+
+# %%
