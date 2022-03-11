eb777da1d512d55246ea8063618572891fc7993d
Alie Lassche <42931944+awlassche@users.noreply.github.com>
2022-03-11 15:55:29 +0100

    Update events_to_txt.py

--- chronicles/parser/events_to_txt.py
+++ chronicles/parser/events_to_txt.py
@@ -1,11 +1,261 @@
+# %%
+import os
+import ndjson
+import re
+from turtle import resetscreen
+from wasabi import msg
+
+import bs4
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+import lxml
+
+
+def detect_date_locator(tag):
+    '''
+    Help function, finding key to extract date tag from an annotated field.
+
+    line : bs4.element.Tag
+        can behave like a list 
+    '''
+    # get correct tag to use
+    if tag.has_attr('datum'):
+        locator = 'datum'
+    elif tag.has_attr('when'):
+        locator = 'when'
+    else:
+        locator = None
+    
+    return locator
+
+def extract_date_attr(tag):
+    '''
+    Help function, actually extract date labels
+
+    line : bs4.element.Tag
+        can behave like a list 
+    '''
+
+    # extract, depending if list or not
+    dates = []
+    for date in tag:
+        locator = detect_date_locator(date)
+        if locator:
+            dates.append(date[locator])
+
+    return dates
+
+
+def delimitation_experiment1(increments):
+    '''
+    text corresponding to a primitive -> `{date_1} document_1 {date_2} document_2`
+
+    Parameters
+    ----------
+    increment : bs4.element.ResultSet
+        iterable of bs4.element.Tag
+    '''
+    # check input is not completely outlandish
+    assert(isinstance(increments, bs4.element.ResultSet))
+    assert(isinstance(increments[0], bs4.element.Tag))
+
+    docs = [] # dump for the results
+    partial_doc = {} # create an empty extraction doc that gives a False
+    for i, line in enumerate(increments):
+
+        # detect dates in line
+        dates_found = line.find_all('datum')
+
+        # if datum is found, start a new doc
+        if dates_found:
+            # use date tag extraction function to handle exception
+            doc_date = extract_date_attr(dates_found)
+
+            # add cache (PREVIOUS doc) to results
+            docs.append(partial_doc)
+
+            # TODO
+            # here there should be splitting of the text in a single line
+            # text before date tag -> old partial_doc
+            # text after date tag -> new partial_doc
+
+            # TODO
+            # also joining words that are on multiple lines...
+
+            # start a NEW doc (bc date was found)
+            partial_doc = {}
+            text_content = [i.get_text() for i in line]
+            partial_doc['date'] = doc_date
+            partial_doc['text'] = text_content
+
+            # last increment in the file
+            # sometimes needs to be added to last document worked on manually
+            # this happens, when last increment contains a date
+            #     a new partial doc is created in if dates_found:
+            #     but, it is never added to the results list
+            #
+            #     TODO: when these two lines move to the back, 
+            #     number of documents increases by one (1791_Purm_Louw_03.xml).
+            #     Don't know why.
+            if i+1 == len(increments):
+                docs.append(partial_doc)
+
+        else:
+            # if there's something in cache 
+            # aka PREVIOUS doc is not empty = being worked on
+            if partial_doc:
+                # add text lines to it
+                text_content = [i.get_text() for i in line]
+                partial_doc['text'].extend(text_content)
+            
+            # if there's nothing in cache
+            # this happens for first lines of a file, before a date has appears
+            else:
+                # start a fresh doc/cache (seems unnessssary)
+                partial_doc = {}
+                # extract and add
+                try:
+                    text_content = [i.get_text() for i in line]
+                except:
+                    print(line)
+                # fake date. TODO input something else?
+                partial_doc['date'] = 'NaN_before_date'
+                partial_doc['text'] = text_content
+
+    return docs
+
+
+def parse_chronicle(path):
+
+    with open(path, 'r', encoding='utf-8') as f_in:
+        soup = BeautifulSoup(f_in, 'lxml')
+
+    # extract call_nr: {YYYY}_{LOCATION_TAG}_{AUTHOR_TAG}
+    title_tags = soup.find_all('title')
+    # one call_nr must be present in file (see tests/test_parsing)
+    call_nr = title_tags[0].get_text()
+
+    increments = soup.find_all('l')
+    res = delimitation_experiment1(increments)
+    # add source
+    # HACK i really don't like that it's inplace
+    [doc.update({'call_nr': call_nr}) for doc in res]
+
+    return res
 
 # %%
-for i in range(len(chron)):
-    if chron[i]['date'] == 'NaN_before_date':
-        continue
-    else:
-        chrontxt = chron[i]['text']
-        filename = str(chron[i]['call_nr']) + '_' + str(chron[i]['date'])[2:-2] + '.txt'
-        with open(filename, 'w', encoding='utf-8') as fileout:
-            fileout.write('\n'.join(chrontxt))
+
+# create documents per event, and merge hyphen
+data_dir = '/Users/alielassche/documents/github/chronicling-topics/corpus/corpus_220222_annotated'
+
+os.chdir('/Users/alielassche/documents/github/chronicling-topics/corpus/txt')
+xml_paths = [os.path.join(data_dir, path)
+             for path in os.listdir(data_dir) if path.endswith('.xml')]
+
+all_events = []
+for path in tqdm(xml_paths):
+    try:
+        chron = parse_chronicle(path)
+        for i in range(len(chron)):
+            if chron[i]['date'] == 'NaN_before_date':
+                continue
+            else:
+                chrontxt = chron[i]['text']
+                chrontxt = ' '.join(chrontxt)
+                chrontxt = re.sub(r"((¬#?) ?)", "", chrontxt)
+                filename = str(chron[i]['call_nr']) + '_' + str(chron[i]['date'])[2:-2] + '.txt'
+                with open(filename, 'a', encoding='utf-8') as fileout:
+                    fileout.write(chrontxt)
+    except:
+        msg.fail(f'file failed: {path}')
+
+    
 # %%
+import glob
+import nltk
+import re
+import string
+import shutil
+import csv
+from nltk import FreqDist
+
+TOKENIZER = nltk.tokenize.word_tokenize
+
+def is_punct(t):
+    return re.match(f'[{string.punctuation}]+$', t) is not None
+
+# %%
+
+# filter documents with more than 150 words and make mfw list with 250 most common words
+
+words_total = []
+n = 0
+new_dir = ('/Users/alielassche/documents/github/chronicling-topics/corpus/txt_150')
+
+os.chdir('/Users/alielassche/documents/github/chronicling-topics/corpus/txt')
+for doc in glob.glob("*.txt"):
+    text = open(doc, "r", encoding="utf-8").read()
+    words = []
+    for sentence in TOKENIZER(text, language="dutch"):
+        words.extend([w.lower() for w in sentence.split() if not is_punct(w)])
+        words_total.extend([w.lower() for w in sentence.split() if not is_punct(w)])
+    if len(words) >= 150:
+        n += 1
+        shutil.copy(doc, new_dir)
+        print(n)
+
+fd = FreqDist(words_total)
+mfw = fd.most_common(250)
+MFW = csv.writer(open('../mfw_events.csv', 'w', encoding='utf-8'))
+for key, count in mfw:
+    MFW.writerow([key, count])
+
+# %%
+
+# prepare corpus for topic modeling
+
+import gensim
+from gensim.corpora import Dictionary
+from gensim.models.wrappers import LdaMallet
+
+os.chdir('/Users/alielassche/documents/github/chronicling-topics/corpus')
+
+corpus_path = '/text_150/*.txt'
+stopwords_path = '/stopwords/stoplist.txt'
+stopwords = [s.lower() for s in open(stopwords_path, 'r', encoding='utf-8').read().splitlines()]
+
+remove_stopwords = lambda x: [word.lower() for word in x if word.lower() not in stopwords and not is_punct(word) and len(word) > 1]
+
+texts = glob.glob(corpus_path, recursive=False)
+tokenized_texts = [TOKENIZER(open(text, "r", encoding="utf-8").read(), language="dutch") for text in texts]
+tokenized_texts = [remove_stopwords(text) for text in tokenized_texts]
+
+# %%
+
+# settings topic model
+
+no_below = 2
+no_above = 1
+
+n_topics = 20
+iterations = 2000
+optimize_interval = 20
+eval_every = 3
+n_workers = 3
+
+# %%
+
+# create dictionary and corpus
+
+dictionary = Dictionary(tokenized_texts)
+dictionary.filter_extremes(no_below=no_below, no_above=no_above)
+corpus = [dictionary.doc2bow(text) for text in tokenized_texts]
+
+# %%
+lda = LdaMallet('/work/Mallet', 
+                corpus=corpus,
+                id2word=dictionary,
+                num_topics=n_topics, 
+                iterations=iterations,  
+                workers=n_workers,
+                optimize_interval=optimize_interval)
