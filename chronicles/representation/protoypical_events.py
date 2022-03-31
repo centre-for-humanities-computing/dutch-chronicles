6ef97770e5ae465970cf50a00b57504184e1001a
supplyandcommand <42962106+supplyandcommand@users.noreply.github.com>
2022-03-31 16:12:31 +0200

    sorting of representations: wrong file

--- chronicles/representation/protoypical_events.py
+++ chronicles/representation/protoypical_events.py
@@ -38,3 +38,69 @@ def prototype_avg_d(representations, metric='cosine'):
 
     return prototype_doc, uncertainity
 
+# %%
+# add some network shit, 
+# so that we can track
+# - timeseries going between central nodes
+# - timeseries going between secondary to n-ary nodes
+# - timeseries going between the most deviating nodes
+
+# %%
+import re
+import datetime
+from tqdm import tqdm
+import ndjson
+import pandas as pd
+
+# representations
+ev = np.load(
+    '../../lda/representation_final_50.npy',
+    allow_pickle=True
+    )
+
+# primitives
+path_primitives_daily = '../../data/primitives_220331/primitives_corrected_daily.ndjson'
+with open(path_primitives_daily) as fin:
+    primitives_daily = ndjson.load(fin)
+
+
+# %%
+# ensure correct formating
+bad_date_docs = []
+primitives_daily_date_format = []
+for doc in primitives_daily:
+    try:
+        datetime.datetime.strptime(doc['clean_date'], '%Y-%m-%d')
+        primitives_daily_date_format.append(doc)
+    except ValueError:
+        bad_date_docs.append(doc)
+
+# %%
+# sort primitives by date
+sorted_prim = sorted(
+    primitives_daily_date_format,
+    key=lambda t: datetime.datetime.strptime(t['clean_date'], '%Y-%m-%d')
+    )
+
+
+# %%
+# append representation to primitives
+rep_doc_id = [event[0] for event in ev]
+rep_vals =[event[1].tolist() for event in ev]
+
+docs_not_represented = []
+for doc in tqdm(sorted_prim):
+    try:
+        idx = rep_doc_id.index(doc['id'])
+        corresponding_rep = rep_vals[idx]
+        doc['representation'] = corresponding_rep
+    except ValueError:
+        sorted_prim.remove(doc)
+        docs_not_represented.append(doc)
+
+
+
+# %%
+with open('/Users/au582299/Repositories/dutch-chronicles/data/primitives_220331/events_repre.ndjson', 'w') as fout:
+    ndjson.dump(sorted_prim, fout)
+
