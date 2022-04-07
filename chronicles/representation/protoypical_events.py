f8fbd4be84954e8c0a839e5a4290eda046e96640
supplyandcommand <42962106+supplyandcommand@users.noreply.github.com>
2022-04-07 11:03:56 +0200

    working example of prototypical events

--- chronicles/representation/protoypical_events.py
+++ chronicles/representation/protoypical_events.py
@@ -1,7 +1,100 @@
 '''
+Preproblem
+----------
+Documents are cosine similarities to topics.
+Why not just take their embedding?
+[DONE]
 
 
-Course
+Problem statement
+-----------------
+We have vectors representing text documents. 
+They are coordinates in linear space (much like what you get from word2vec).
+
+Each document happened on a day, so our analysis has a daily resolution now. 
+But, we want to be able to represent a larger chunk of time, e.g. a week with a single datapoint.
+To do this, we decided to group documents that are close to each other in time into timebins -> each timebin contains multiple documents. 
+Though not every timebin contains the same ammount of documents! 
+
+A timebin will be represented by its "prototypical document": a single vector (document) that is most representative of the whole timebin (multiple vectors).
+In the end we also need a function that extracts the n-th most representative vector from the timebin (not only the top one).
+
+In cases where it makes sense, we also want to keep track of the uncertainity of the representation = how well does a prototypical document represent its timebin?
+
+
+Solution 1: Spatial distance
+----------------------------
+I already did this.
+Calculates pair-wise distances between vectors using some geometric measure (cosine distance, euclidean, etc.). 
+Most representative vector = one with lowest avg distance to all other vectors in a timebin.
+Uncertainity = standard deviation of distances from the most representative vector to the others 
+
+
+Solution 2: Networks
+--------------------
+
+1) figure out how to convert the vectors into a network 
+    1a) so documents are nodes connected by edges
+    1b) possible solution: add nodes one at a time. Connect each new node to the most similar existing node. Keep track of distance (could be used as weight of the edge)
+        - pros: network with desirable properitiers – not all nodes are connected
+        - cons: unstable. Could get different networks depending on which node you start with.
+    
+    1c) possible solution 2: add all nodes, connect all. Use distance between vectors as weights
+
+2) how to rank nodes by representativeness?
+    2a) perhaps using network measures – eigen centrality? Avg distance to other nodes? Avg path length?
+
+3) how to track uncertainity?
+    3a) ???
+
+
+Solution 3: Entropy
+-------------------
+
+1) pretend that the vectors are probability distributions 
+    1a) normalize, so that sum(vector) == 1
+
+2) use an information-theoretic measure to measure difference between vectors
+    2b) Jensen-Shanon Divergence would be an obvious choice 
+    2c) interpretation: given we expect vector P to happen, how suprising is vector Q?
+
+3) rank vectors by representativeness
+    3a) lowest avg relative entropy (least surprising) = most represenative?
+
+4) track uncertainity: standard deviation of relative entropies?
+
+
+Solution 4: model based
+-----------------------
+
+1) use a probabilistic model to model a timebin
+    1a) ??? 
+
+2) use model to predict each vector, track prediction error for each vector
+
+3) rank vectors by representativeness
+    3a) vector with the lowest prediction error = most representative?
+
+4) track uncertanity: just take prediction error
+
+
+Data format
+-----------
+List[dict], where dict is a timebin.
+
+dict has following keys:
+
+    timebin : int (may change, but will be single element)
+    dates : List[str]
+    call_nrs : List[str]
+    ids : List[int]
+    vectors : List[List[float]]
+
+'''
+
+
+'''
+In this script (old section)
 ------
 a) Get a bin of representations
 b) Calculate distances to other docs
