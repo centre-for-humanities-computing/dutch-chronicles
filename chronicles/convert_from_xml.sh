e3023c8899419d558d0c3782f8d1ab1da01ad847
Alie Lassche <42931944+awlassche@users.noreply.github.com>
2022-05-06 09:28:20 +0200

    Update convert_from_xml.sh

--- chronicles/convert_from_xml.sh
+++ chronicles/convert_from_xml.sh
@@ -1,19 +1,19 @@
 ###
 ### INPUT PARAMS: change as you please!
 ###
-dir_corpus_annotated='../data/corpus_220329_annotated'
-dir_corpus_corrected='../data/corpus_220329_corrected'
-run_when='220331'
+dir_corpus_annotated='../../chronicling-topics/corpus/corpus_220503/corpus_220503_annotated'
+dir_corpus_corrected='../../chronicling-topics/corpus/corpus_220503/corpus_220503_corrected'
+run_when='220503'
 
 
 ###
 ### SETUP: does output dir exist? 
 ###
-if [ -d "../data/primitives_${run_when}" ] 
+if [ -d "../../chronicling-topics/primitives_${run_when}" ] 
 then
     echo "Directory ${wdir} exists." 
 else
-    mkdir -p "../data/primitives_${run_when}"
+    mkdir -p "../../chronicling-topics/primitives_${run_when}"
     echo "Creating ${run_when} output dir in ../data/"
 fi
 
@@ -26,25 +26,25 @@ echo "Parsing corpus_annotated"
 python parser/xml_parsing.py \
     -d "${dir_corpus_annotated}" \
     -s 1 \
-    -o "../data/primitives_${run_when}/primitives_annotated.ndjson"
+    -o "../../chronicling-topics/primitives_${run_when}/primitives_annotated.ndjson"
 
 echo "Parsing corpus_corrected"
 python parser/xml_parsing.py \
     -d "${dir_corpus_corrected}" \
     -s 1 \
-    -o "../data/primitives_${run_when}/primitives_corrected.ndjson"
+    -o "../../chronicling-topics/primitives_${run_when}/primitives_corrected.ndjson"
 
 echo "Generating IDs"
 python parser/give_ids.py \
-    -ap "../data/primitives_${run_when}/primitives_annotated.ndjson" \
-    -cp "../data/primitives_${run_when}/primitives_corrected.ndjson"
+    -ap "../../chronicling-topics/primitives_${run_when}/primitives_annotated.ndjson" \
+    -cp "../../chronicling-topics/primitives_${run_when}/primitives_corrected.ndjson"
 
 echo "Cleaning date tags on corrected corpus"
 python misc/date_tag_resolutions.py \
-    -i "../data/primitives_${run_when}/primitives_corrected.ndjson" \
-    -o "../data/primitives_${run_when}/primitives_corrected_daily.ndjson"
+    -i "../../chronicling-topics/primitives_${run_when}/primitives_corrected.ndjson" \
+    -o "../../chronicling-topics/primitives_${run_when}/primitives_corrected_daily.ndjson"
 
 echo "Cleaning date tags on annotated corpus"
 python misc/date_tag_resolutions.py \
-    -i "../data/primitives_${run_when}/primitives_annotated.ndjson" \
-    -o "../data/primitives_${run_when}/primitives_annotated_daily.ndjson"
+    -i "../../chronicling-topics/primitives_${run_when}/primitives_annotated.ndjson" \
+    -o "../../chronicling-topics/primitives_${run_when}/primitives_annotated_daily.ndjson"
